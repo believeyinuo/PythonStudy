5e3908c42eef511a91e1231bffc4f8167213a43c
believeyinuo <believeyinuo@163.com>
2019-09-04 22:56:51 +0800

    web

---
+++ WEB_AUTO_Test/web_driver_83/learn_web_driver.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+#@Time : 2019-09-04 
+#@Author：lqc
+#@Email:572948875@qq.com
+#File : learn_web_driver.py
+
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="usr/LiQingChun/download")
+
+
+
