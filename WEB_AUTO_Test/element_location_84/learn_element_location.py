d1f9639a3fda7f43af5d63a2c72f020e0025e2df
believeyinuo <believeyinuo@163.com>
2019-09-05 23:27:21 +0800

    web元素定位

---
+++ WEB_AUTO_Test/element_location_84/learn_element_location.py
@@ -0,0 +1,50 @@
+# -*- coding:utf-8 -*-
+#@Time : 2019-09-05 
+#@Author：lqc
+#@Email:572948875@qq.com
+#File : learn_element_location.py
+
+# 元素定位
+# id、classname、tagname、name
+
+from selenium import webdriver
+
+# 启动谷歌浏览器，开启与浏览器之间的会话
+driver = webdriver.Chrome()
+
+# 访问一个网页
+driver.get("https://www.baidu.com")
+
+# 方式一
+ele = driver.find_element_by_id("kw")
+print(ele.get_attribute("class"))
+print(ele)
+
+# 方式二
+eles = driver.find_elements_by_class_name("s_ipt")
+driver.find_element_by_class_name("s_ipt")
+
+# 方式三
+driver.find_element_by_name("wd")
+driver.find_elements_by_name("wd")
+
+# 方式四
+driver.find_element_by_tag_name("input")
+driver.find_elements_by_tag_name("input")
+
+# 方式五、六 针对链接
+driver.find_element_by_link_text("更多产品")
+driver.find_elements_by_link_text("更多产品")
+driver.find_element_by_partial_link_text("更多产品")
+driver.find_elements_by_partial_link_text("更多产品")
+
+# xpath
+# driver.find_element_by_xpath("")
+# 绝对定位
+#      /html/body/div[2]/div/form/div[1]/input
+#      以/开头
+#      非常依赖于页面的顺序和位置
+# 相对定位
+#       //*[@id="kw"]
+#       以//开头
+#       不依赖页面的的顺序和位置。只看整个页面当中有没有符合表达式的元素
--- WEB_AUTO_Test/web_driver_83/learn_web_driver.py
+++ WEB_AUTO_Test/web_driver_83/learn_web_driver.py
@@ -6,7 +6,36 @@
 
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path="usr/LiQingChun/download")
+# 启动谷歌浏览器，开启与浏览器之间的会话   83讲的
+# driver = webdriver.Chrome(service_log_path="/Users/LiQingChun/Desktop/logs")
+driver = webdriver.Chrome()
 
+# 访问一个网页
+driver.get("https://www.baidu.com")
+
+# 窗口最大化
+driver.maximize_window()
+
+driver.get("http://www.taobao.com")
+# 回退上一页
+driver.back()
+
+# 前进到下一页
+driver.forward()
+
+# 刷新
+driver.refresh()
+
+# 获取标题
+print(driver.title)
+
+# 获取网址
+print(driver.current_url)
+
+# 窗口的句柄
+print(driver.current_window_handle)
+
+# 结束会话
+# driver.quit()
 
 
